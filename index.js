9d4477d547aa466ae7a917da70e4ddd72ecdfbc5
FredDESCAMPS <yadunews@gmail.com>
2025-07-13 00:52:36 +0200

    index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const axios = require('axios');
-const fetch = require('node-fetch');
+
 const base64 = require('base-64');
 
 const app = express();
